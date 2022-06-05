f621f2021cd1d50bbdcfff69f1dfe6f4c5f0bdb0
gayathri994 <106674660+gayathri994@users.noreply.github.com>
2022-06-05 12:42:35 +0530

    Add files via upload

---
+++ int.py
@@ -0,0 +1,11 @@
+
+def great():
+    print("enter input for a")
+    a = int(input())
+    print("enter input for b")
+    b = int(input())
+    if a>b:
+        print("true")
+    else :
+        print("false")
+great()
