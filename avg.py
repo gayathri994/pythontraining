1400d9447efd64edeb631bfbf6c59be8db068b8d
gayathri994 <106674660+gayathri994@users.noreply.github.com>
2022-06-05 11:43:02 +0530

    Add files via upload

---
+++ avg.py
@@ -0,0 +1,9 @@
+def avg_of_three():
+    print("given input for x")
+    x = int(input())
+    print("given input for y")
+    y = int(input())
+    print("given input for z")
+    z = int(input())
+print("avg of three elements:",(x+y+z/3)
+avg_of_three()
