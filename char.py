0b05ea7723d00183bc02e46064909b6b62dd02fb
gayathri994 <106674660+gayathri994@users.noreply.github.com>
2022-06-07 12:13:08 +0530

    Add files via upload

---
+++ char.py
@@ -0,0 +1,8 @@
+def char():
+    g = "abcdefmnacyxh"
+    count = 0
+    for i in g:
+         if(i=='a') :
+               count = count+1
+    print('no.of times that letter"a"found in given list:',count)
+char()
