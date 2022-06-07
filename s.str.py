07adebadb3fe5b1f55325fb24fe1d0b23972ece7
gayathri994 <106674660+gayathri994@users.noreply.github.com>
2022-06-07 12:12:39 +0530

    Add files via upload

---
+++ s.str.py
@@ -0,0 +1,9 @@
+def substring():
+    s = "abcdefmnacyxabchjmlabcjjjkh"
+    ss = "mnay"
+    count = 0
+    for i in s:
+        if (i=="mnay"):
+            count = count+1
+    print(count)
+substring()
