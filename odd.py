db22cefb73dcf09a60daf7cd2f839a6afce9a63a
gayathri994 <106674660+gayathri994@users.noreply.github.com>
2022-06-14 18:59:16 +0530

    Add files via upload

--- odd.py
+++ odd.py
@@ -1,11 +1,15 @@
-def odd_in_list():
-    g = [1,11,7,8,-1,6]
+
+def odd_in_list(g,b):
     c = []
     for i in g :
          if i%2==1 :
             c.append(i)
-    print("elements less than given value")
-    print(c)
-    print("number of elements that are less than given number")
-    print(len(c))
-odd_in_list()
+    return c
+if odd_in_list([1,11,7,8,-1,6],(11))==[1,11,7,-1]:
+    print("test passed")
+else:
+    print("test failed")
+if odd_in_list([6,8,2],())==[]:
+    print("test passed")
+else:
+    print("test failed")
