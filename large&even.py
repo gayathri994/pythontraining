198cc90b41f8a7b3949056cc8079f3841f0a6c2b
gayathri994 <106674660+gayathri994@users.noreply.github.com>
2022-06-14 18:46:13 +0530

    Add files via upload

--- large&even.py
+++ large&even.py
@@ -1,9 +1,15 @@
-def largest_even():
-    g = [1,11,17,28,-1,6]
+#Write a function which return max even element in a given list
+def largest_even(g):
     largest = 0
-    for i in g :
+    for i in g:
         if i > largest and i%2==0 :
             largest = i
-    print(largest)
-largest_even()
-
+    return largest
+if largest_even ([1,11,7,8,-1,6])==8:
+    print("pass")
+else:
+    print("fail")
+if largest_even ([1,11,17,28,-1,6])==28:
+    print("pass")
+else:
+    print("fail")
