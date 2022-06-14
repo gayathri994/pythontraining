7f5e7674f79bf5b3cb93234356ac12934665d14c
gayathri994 <106674660+gayathri994@users.noreply.github.com>
2022-06-14 18:42:37 +0530

    Delete avg.py

--- avg.py
+++
@@ -1,9 +0,0 @@
-def avg_of_three():
-    print("given input for x")
-    x = int(input())
-    print("given input for y")
-    y = int(input())
-    print("given input for z")
-    z = int(input())
-print("avg of three elements:",(x+y+z/3)
-avg_of_three()
