7ffdff016349a29c6bf166d5fada41477e182bf3
gayathri994 <106674660+gayathri994@users.noreply.github.com>
2022-06-14 18:56:11 +0530

    Add files via upload

--- less&odd.py
+++ less&odd.py
@@ -1,13 +1,22 @@
-def less_than_and_odd():
-    g = [1,11,7,8,-1,6]
-    b = 10
-    print("value that u need to check less than",b)
+def less_or_equal_odd(g,b):
     c = []
-    for i in g :
-        if i<=b and i%2==1 :
+    for i in g:
+        if i<=b and  i%2==1 :
             c.append(i)
-    print("elements less than and odd for given value")
-    print(c)
-    print("no.of elements that are less than and odd")
-    print(len(c))
-less_than_and_odd()
+    return len(c)
+if less_or_equal_odd([1,11,7,8,-1,6],(11))==4:
+    print("test passed")
+else:
+    print("test failed")
+if less_or_equal_odd([1,11,7,8,-1,6],(18))==4:
+    print("test1 passed")
+else:
+    print("test1 failed")
+if less_or_equal_odd([1,11,7,8,-1,6],(-1))==1:
+    print("test2 passed")
+else:
+    print("test2 failed")
+if less_or_equal_odd([1,11,7,8,-1,6],(-2))==0:
+    print("test3 passed")
+else:
+    print("test3 failed")
