fd9cb3cd356586fb233b7eb31480be1ecd8e609f
gayathri994 <106674660+gayathri994@users.noreply.github.com>
2022-06-14 18:51:55 +0530

    Add files via upload

--- large.py
+++ large.py
@@ -1,9 +1,15 @@
-def largest():
-    g = [1,11,17,28,-1,6]
-    largest = 0
-    for i in g :
-        if i > largest :
-            largest = i
-    print(largest)
-largest()
 
+def largest(g):
+    c = 0
+    for i in g :
+        if i > c :
+            c = i
+    return c
+if largest([1,11,17,28,-1,6])==28:
+    print("test passed")
+else:
+    print("test failed")
+if largest([1,11,8,-1,6,7])==11:
+    print("test1 passed")
+else:
+    print("test1 failed")
